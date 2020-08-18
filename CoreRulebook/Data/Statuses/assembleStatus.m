b2184d0c6d1ec8d37d728398724df2ef2eb74858
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2020-08-18 16:02:51 +0100

    Statuses reintroduced

---
+++ CoreRulebook/Data/Statuses/assembleStatus.m
@@ -0,0 +1,46 @@
+function assembleStatus(fileRoot)
+
+    if nargin < 1
+        disp('Insufficient inputs provided');
+        addpath('../Functions/');
+        fileRoot = '../../';
+    end
+
+    statuses= readtable('Statuses.xlsx');
+    statuses = sortrows(statuses);
+
+    
+    text = "";
+
+    n = size(statuses);
+    W= width(statuses);
+    for (i = 1:n)
+        
+        r = statuses{i,3}{1};
+        line = "\status{" + statuses.Name{i} + "}{";
+        line = line + statuses{i,2}{1} + "}{";
+        
+        j =3;
+         r = statuses{i,j}{1};
+            while j <= W && ~isempty(r) 
+               
+                line = line + "\item " + r;
+                j = j +1;
+                
+                r = "";
+                if j <= W
+                    r = statuses{i,j}{1};
+                end
+  
+            end
+  
+        line = line + "}";
+        text = text + prepareText(line) + "\n\n";
+    end
+
+    fileName = strcat(fileRoot,'Chapters/Part2_Actions/StatusList.tex');
+   
+
+    FID = fopen(fileName,'w');
+    fprintf(FID, text);
+end
